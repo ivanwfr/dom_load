0c573ecd89c7df93f78f50499bf644c350cc29c9
ivan Wellesz <ivanw@orange.fr>
2022-02-09 13:38:55 +0100

    inlining drag_cursor (was missing)

--- script/dom_tools.js
+++ script/dom_tools.js
@@ -55,7 +55,7 @@
 /* eslint-disable no-warning-comments */
 
 const DOM_TOOLS_JS_ID       = "dom_tools_js" ;
-const DOM_TOOLS_JS_TAG      = DOM_TOOLS_JS_ID   +" (220208:21h:29)";
+const DOM_TOOLS_JS_TAG      = DOM_TOOLS_JS_ID   +" (220209:13h:31)";
 /*}}}*/
 let dom_tools   = (function() {
 "use strict";
@@ -24098,6 +24098,91 @@ log("t_show_SNAPSHOT: snapshot_mail_body=["+t_util.ellipsis(snapshot_mail_body)+
 ../script/drag_cursor.js
 */
 /*INLINE{{{*/
+/*INLINE{{{*/
+let drag_cursor  = (function() {
+/*"use strict";*/
+
+/*{{{*/
+let      drag_cursor_div;
+
+/*
+let      drag_cursor_count = 0;
+*/
+/*}}}*/
+/*_ set_mouseUP_display_state  {{{*/
+/*{{{*/
+let mouseUP_display_state = true;
+
+/*}}}*/
+let set_mouseUP_display_state = function(state=true)
+{
+    mouseUP_display_state = state;
+
+    if(state) show_drag_cursor();
+
+    if(state) t_CURSOR_add_MOVE_LISTENER();
+    else      t_CURSOR_del_MOVE_LISTENER();
+};
+/*}}}*/
+let get_mouseUP_display_state = function() { return mouseUP_display_state; };
+/*_ show_drag_cursor {{{*/
+let show_drag_cursor = function()
+{
+/*{{{
+console.log("%c show_drag_cursor", lfX[++drag_cursor_count % 10], "onMoveDXY:",onMoveDXY , "onDown_XY:",onDown_XY)
+}}}*/
+    if(!drag_cursor_div) {
+        drag_cursor_div = document.createElement("DIV");
+
+        drag_cursor_div.id                    =     "drag_cursor";
+        drag_cursor_div.style.pointerEvents   =            "none";
+        drag_cursor_div.style.position        =           "fixed";
+        drag_cursor_div.style.margin          =             "0px";
+        drag_cursor_div.style.padding         =            "16px";
+        drag_cursor_div.style.backgroundColor =            "#FF0";
+        drag_cursor_div.style.border          =  "3px solid #000";
+        drag_cursor_div.style.borderRadius    = "1em 0em 1em 1em";
+        drag_cursor_div.style.zIndex          =      "2147483647";
+        drag_cursor_div.style.opacity         =             "0.5";
+
+        document.documentElement.appendChild( drag_cursor_div );
+    }
+    drag_cursor_div.style.left    = (onDown_XY.x - drag_cursor_div.offsetWidth)+"px";
+    drag_cursor_div.style.top     = (onDown_XY.y                         )+"px";
+    drag_cursor_div.style.display = "block";
+};
+/*}}}*/
+/*_ move_drag_cursor {{{*/
+let move_drag_cursor = function(e)
+{
+    if(!drag_cursor_div                         ) return;
+    if(!drag_cursor_div.style.display == "block") return;
+
+    let      xy = t_util.get_event_XY(e);
+    drag_cursor_div.style.left    = (xy.x - drag_cursor_div.offsetWidth)+"px";
+    drag_cursor_div.style.top     = (xy.y                         )+"px";
+};
+/*}}}*/
+let hide_drag_cursor                     = function() { if(drag_cursor_div) drag_cursor_div.style.display = "none"; };
+let add_drag_cursor_CSS_NOT_MOVED_ENOUGH = function() { if(drag_cursor_div) drag_cursor_div.classList.add   (CSS_NOT_MOVED_ENOUGH); };
+let del_drag_cursor_CSS_NOT_MOVED_ENOUGH = function() { if(drag_cursor_div) drag_cursor_div.classList.remove(CSS_NOT_MOVED_ENOUGH); };
+let add_drag_cursor_CSS_MOVE_ON_COOLDOWN = function() { if(drag_cursor_div) drag_cursor_div.classList.add   (CSS_MOVE_ON_COOLDOWN); };
+let del_drag_cursor_CSS_MOVE_ON_COOLDOWN = function() { if(drag_cursor_div) drag_cursor_div.classList.remove(CSS_MOVE_ON_COOLDOWN); };
+
+/* EXPORT */
+return { name : "drag_cursor"
+    ,    set_mouseUP_display_state
+    ,    get_mouseUP_display_state
+    ,    show_drag_cursor
+    ,    hide_drag_cursor
+    ,    move_drag_cursor
+    ,    add_drag_cursor_CSS_MOVE_ON_COOLDOWN
+    ,    add_drag_cursor_CSS_NOT_MOVED_ENOUGH
+    ,    del_drag_cursor_CSS_MOVE_ON_COOLDOWN
+    ,    del_drag_cursor_CSS_NOT_MOVED_ENOUGH
+};
+}());
+/*INLINE}}}*/
 /*}}}*/
 
 /* EXPORT tools */
